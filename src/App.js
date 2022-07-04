1a057d55326aaf1a0dcc204db59cfb2913a0b693
VitorCarvalho1 <vitorhugo.carvalho2@yahoo.com.br>
2022-07-04 06:19:08 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import LogoPokemon from './assets/International_Pokémon_logo.svg.png'
+import './App.css'
+import Api from './Api'
+
+
+export default function Home(){
+
+  
+
+  return (
+    <header>
+        <img src={LogoPokemon}/>
+    </header>
+  )
+}
